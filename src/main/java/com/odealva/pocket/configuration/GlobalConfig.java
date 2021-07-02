e53f37eb261b02b5aabc2433acef0770d6d5f82a
Oscar De Alva <oscar.ivan98@gmail.com>
2021-07-02 10:45:02 -0500

    Added logging when the configs load

--- src/main/java/com/odealva/pocket/configuration/GlobalConfig.java
+++ src/main/java/com/odealva/pocket/configuration/GlobalConfig.java
@@ -1,16 +1,23 @@
 package com.odealva.pocket.configuration;
 
 
+import com.odealva.pocket.Main;
 import com.odealva.pocket.configuration.yaml.YamlPropertySourceFactory;
 import com.odealva.pocket.configuration.credentials.PocketCredentials;
 import com.odealva.pocket.configuration.credentials.TelegramCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @PropertySource(value = "classpath:app-config.yaml", factory = YamlPropertySourceFactory.class)
 public class GlobalConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalConfig.class);
 
     @Bean("msgsConfig")
     public MessagesConfig msgsConf() {
@@ -30,6 +37,12 @@ public class GlobalConfig {
     @Bean("telegramConf")
     public TelegramCredentials telegram() {
         return new TelegramCredentials();
+    }
+
+    @PostConstruct
+    private void configRead() {
+        logger.debug("Loaded the following config: ");
+        logger.debug(this.toString());
     }
 
     @Override
