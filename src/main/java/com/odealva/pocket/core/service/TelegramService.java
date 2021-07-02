5190ca64a46eef38542b0d89b757e9246f69b83e
Oscar De Alva <oscar.ivan98@gmail.com>
2021-07-02 10:42:36 -0500

    Made return type of sendArticles more explicit of results

--- src/main/java/com/odealva/pocket/core/service/TelegramService.java
+++ src/main/java/com/odealva/pocket/core/service/TelegramService.java
@@ -2,14 +2,12 @@ package com.odealva.pocket.core.service;
 
 import com.odealva.pocket.configuration.credentials.TelegramCredentials;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
-import io.vavr.CheckedFunction1;
-import io.vavr.Function1;
-import io.vavr.Tuple;
-import io.vavr.Tuple2;
+import io.vavr.*;
 import io.vavr.control.Try;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -25,12 +23,12 @@ public class TelegramService {
     @Autowired
     private TelegramBot bot;
 
-    public Try<Tuple2<Boolean, Message>> sendArticles(String articles) {
+    public Try<Tuple2<Integer, Chat>> sendArticles(String articles) {
         SendMessage msg = new SendMessage(config.getChatId(), articles).parseMode(ParseMode.Markdown);
         Function1<SendMessage, Try<SendResponse>> sendMsg = CheckedFunction1.liftTry(bot::execute);
 
         return sendMsg
                 .apply(msg)
-                .map(res -> Tuple.of(res.isOk(), res.message()));
+                .map(res -> Tuple.of(res.message().messageId(), res.message().chat()));
     }
 }
